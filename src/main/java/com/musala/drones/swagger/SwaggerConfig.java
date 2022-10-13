2a7bbc1edd690a7379a062272a933d547dda868d
Ikechukwu Ezugworie <ikechukwue@hardcorebiometric.com>
2022-10-13 13:16:22 +0100

    fix: fixed server bug port on swagger

--- src/main/java/com/musala/drones/swagger/SwaggerConfig.java
+++ src/main/java/com/musala/drones/swagger/SwaggerConfig.java
@@ -16,6 +16,6 @@ import io.swagger.v3.oas.annotations.servers.Server;
                         email = "i.ezugworie@gmail.com"
                 )
         ),
-        servers = @Server(url = "http://localhost:8085")
+        servers = @Server(url = "http://localhost:8080")
 )
 public class SwaggerConfig {}
